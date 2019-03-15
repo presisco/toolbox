56792abe1c19036fff1f3eb5818c75a38a815985
presisco <internight@sina.com>
2019-03-15 10:55:55 +0800

    adjust TelnetHelper functions

--- src/main/kotlin/com/presisco/toolbox/net/TelnetHelper.kt
+++ src/main/kotlin/com/presisco/toolbox/net/TelnetHelper.kt
@@ -1,61 +1,53 @@
 package com.presisco.toolbox.net
 
 import org.apache.commons.net.telnet.TelnetClient
+import java.io.BufferedWriter
 import java.io.InputStream
-import java.io.OutputStream
 
 class TelnetHelper(
         private val host: String,
         private val port: Int = 23,
         private val code: String = "",
-        private val bufferSize: Int = 4096,
         private val cmdIntervalMs: Long = 100
 ) {
     private val telnetClient = TelnetClient()
-    private val keyboard: OutputStream
+    private val keyboard: BufferedWriter
     private val screen: InputStream
-    private val screenSpace = ByteArray(bufferSize)
 
     init {
         telnetClient.connect(host, port)
-        keyboard = telnetClient.outputStream
+        keyboard = telnetClient.outputStream.bufferedWriter()
         screen = telnetClient.inputStream
     }
 
-    fun waitForMs(ms: Long) {
-        Thread.sleep(ms)
+    fun waitCmdInterval() {
+        Thread.sleep(cmdIntervalMs)
     }
 
-    fun read(expect: String = "\n"): String {
-        var offset = 0
+    fun read(expect: String): String {
+        val screenBuffer = StringBuffer()
         while (true) {
-            val count = screen.read(screenSpace, offset, bufferSize - offset)
-            if (count > 0) {
-                if (String(screenSpace, offset, count).contains(expect)) {
-                    return String(screenSpace, 0, offset + count)
-                }
-                offset += count
+            val available = screen.available()
+            val buffer = ByteArray(available)
+            screen.read(buffer)
+            val newText = String(buffer)
+            screenBuffer.append(newText)
+
+            if (screenBuffer.contains(expect)) {
+                return screenBuffer.toString()
             }
         }
     }
 
     fun send(vararg cmdList: Pair<String, String>): String {
-        var output = ""
+        val sb = StringBuilder()
         cmdList.forEach {
-            keyboard.write("${it.first}\n".toByteArray())
+            keyboard.write("${it.first}\n")
             keyboard.flush()
-            output = read(it.second)
-            waitForMs(cmdIntervalMs)
+            sb.append(read(it.second))
+            waitCmdInterval()
         }
-        return output
-    }
-
-    fun login(user: String, password: String): String {
-        read("login:")
-        waitForMs(cmdIntervalMs)
-        send(Pair(user, "Password:"))
-        waitForMs(cmdIntervalMs)
-        return send(Pair(password, "$user@"))
+        return sb.toString()
     }
 
     fun close() {
